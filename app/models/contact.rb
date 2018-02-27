0cb5d0fcfcd9c577813dda7ac1cecd824d8c70cc
Devin Parkinson <dparky13@icloud.com>
2018-02-27 15:38:07 -0700

    Delete, Edit Functions Available

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,10 @@
 class Contact < ApplicationRecord
+  validates :age, numericality: true 
+  validates :name, :eye_color, :hair_color, length: { in: 2..25,
+    message: "Please put a valid entry"
+  }
+  validates :gender, inclusion: {
+    in: %w(Male Female),
+    message: "Please either enter Male or Female"
+  }
 end
